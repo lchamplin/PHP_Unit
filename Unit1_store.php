3b7cd646b74db4b388f73bfa8a39b864ce0eaec0
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-10-24 19:27:52 -0600

    trying again

--- Unit1_store.php
+++ Unit1_store.php
@@ -18,12 +18,14 @@
 <form action="Unit1_process_order.php" method="post">
         <br>
         <personal>
+                <br>
                 First Name: <input type="text" name="fname" required><br>
                 Last Name: <input type="text" name="lname" required><br>
                 E-mail: <input type="email" name="email" required><br>
         </personal>
 
-        <product>
+        <product style="border: 0.25vw red dashed; ">
+                <br>
                 <select name="products" required>
                         <option value="" disabled selected hidden>Choose a product</option>
                         <option value="coffee">Gummy Bears</option>
