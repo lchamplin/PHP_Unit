4439a963063997567f2e9fa0e48bba7ea199dfe9
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-10-25 12:17:31 -0600

    new css formating and tags

--- Unit1_store.php
+++ Unit1_store.php
@@ -17,14 +17,17 @@
 
 <form action="Unit1_process_order.php" method="post">
         <br>
-        <personal>
+        <div class="personal">
+        <fieldset class="personal">
                 <br>
                 First Name: <input type="text" name="fname" required><br>
                 Last Name: <input type="text" name="lname" required><br>
                 E-mail: <input type="email" name="email" required><br>
-        </personal>
+        </fieldset>
+        </div>
 
-        <product style="border: 0.25vw red dashed; ">
+          <div class="product">
+   <fieldset class="product">
                 <br>
                 <select name="products" required>
                         <option value="" disabled selected hidden>Choose a product</option>
@@ -32,7 +35,7 @@
                         <option value="tea">Chocolates</option>
                         <option value="milk">Caramels</option>
                 </select>
-                Quantity: <input type="number" min=1 required><br>
+                Quantity: <input type="number" min=1 max=100 required><br>
                 <p>Would you like to round up to donate?</p>
                 <span>
                         <input type="radio" id="yes" name="donate" value="Yes">
@@ -40,9 +43,13 @@
                         <input type="radio" id="css" name="donate" value="No" checked>
                         <label for="No">No</label><br>
                 </span>
-        </product>
+</fieldset>
+</div>
         <input type="submit">
 </form>
+
+<img height=100 width=100 id="image">
+
 
 </body>
 </html>
