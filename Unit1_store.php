17d76c6873cbdcae14cfb54215145e0058fa4ca4
Lauren Champlin <lchamplin@mymail.mines.edu>
2021-10-24 18:53:27 -0600

    more css again

--- Unit1_store.php
+++ Unit1_store.php
@@ -17,13 +17,13 @@
 
 <form action="welcome.php" method="post">
         <br>
-        <div id="personal">
+        <div class="personal">
                 First Name: <input type="text" name="fname" required><br>
                 Last Name: <input type="text" name="lname" required><br>
                 E-mail: <input type="email" name="email" required><br>
         </div>
 
-        <div id="product">
+        <div class="product">
                 <select name="products" required>
                         <option value="" disabled selected hidden>Choose a product</option>
                         <option value="coffee">Gummy Bears</option>
@@ -35,7 +35,7 @@
                 <span>
                         <input type="radio" id="yes" name="donate" value="Yes">
                         <label for="Yes">Yes</label><br>
-                        <input type="radio" id="css" name="donate" value="No">
+                        <input type="radio" id="css" name="donate" value="No" checked>
                         <label for="No">No</label><br>
                 </span>
         </div>
